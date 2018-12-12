865cbc133075ef24c7a2727147f6dde437993159
Sairaj Sawant <40688966+Castlesai@users.noreply.github.com>
2018-12-13 00:12:15 +0530

    Create listmutator.py

---
+++ listmutator.py
@@ -0,0 +1,9 @@
+/*list mutatior add element to list at specific location */
+
+
+def mutate_string(string, position, character):
+    l=list(string)
+    l[position]=character
+    string="".join(l)
+
+    return string
